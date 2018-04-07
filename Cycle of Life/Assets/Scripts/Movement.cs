beb17bacfe0939cee827b8ba0540f4a7658e73f1
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-04-07 16:54:49 -0400

    added basic AI, animation control

--- Cycle of Life/Assets/Scripts/Movement.cs
+++ Cycle of Life/Assets/Scripts/Movement.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
     public float speed;
+    bool facingRight;
     Rigidbody2D playerBody;
+    Animator playerAnimation;
 	// Use this for initialization
 	void Start () {
         playerBody = GetComponent<Rigidbody2D>();
+        //playerAnimation = GetComponent<Animator>();
+        facingRight = true; 
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float horizontal = Input.GetAxis("Horizontal");
+        //playerAnimation.SetFloat("speed", Mathf.Abs(horizontal));
         float vertical = Input.GetAxis("Vertical");
         handleMovement(horizontal,vertical);
+       
 	}
     void handleMovement(float directionX,float directionY)
     {
         playerBody.velocity = new Vector2(directionX * speed, directionY* speed);
+               
     }
 
 }
---
+++ Cycle of Life/Assets/Scripts/NPCscript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCscript : MonoBehaviour {
+
+    public Transform[] patrolPoints;
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    Transform currentPatrolPoint;
+    int currentPatrolIndex;
+    int randomSpot;
+
+	// Use this for initialization
+	void Start () {
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, patrolPoints.Length);
+        currentPatrolIndex = 0;
+       // currentPatrolPoint = patrolPoints[currentPatrolIndex];
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[randomSpot].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, patrolPoints[randomSpot].position) < 0.2f)
+        {
+            if(waitTime <=0)
+            {
+                randomSpot = Random.Range(0, patrolPoints.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        //transform.Translate(Vector2.right * Time.deltaTime * speed);
+        //if(Vector3.Distance(transform.position,currentPatrolPoint.position) < 0.1f)
+        //{
+        //    if(currentPatrolIndex +1 < patrolPoints.Length)
+        //    {
+        //        currentPatrolIndex++;
+        //    }
+        //    else
+        //    {
+        //        currentPatrolIndex = 0;
+        //    }
+        //    currentPatrolPoint = patrolPoints[currentPatrolIndex];
+        //}
+
+        //Turn to face the current patrol point
+        //finding the        //Vector2 patrolPointDir = currentPatrolPoint.position - transform.position;
+        ////figure out the rotation in degrees that we need to turn towards
+        //float angle = Mathf.Atan2(patrolPointDir.y, patrolPointDir.x)* Mathf.Rad2Deg + 90f;
+        ////Made the rotation  that we need to face
+
+        //Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+        ////Apply the rotation to out transform
+        //transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90f); direction vector that points to the patrol point
+
+	}
+}
