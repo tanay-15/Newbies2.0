dce4a0c8c3d8e25d29ed462d6db1a8e7a14842d6
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-04-06 23:56:04 -0400

    worked on GUI.

---
+++ Cycle of Life/Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour {
+    public Text timerText;
+    public float myTimer = 60;
+	// Use this for initialization
+	void Start () {
+        timerText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTimer -= Time.deltaTime;
+        timerText.text = myTimer.ToString("f0"); //f0 is for the number of decimal places.
+        print(myTimer);
+	}
+ 
+}
---
+++ Cycle of Life/Assets/Scripts/canvasControl.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class canvasControl : MonoBehaviour {
+    
+    [SerializeField]
+    private CanvasGroup initialCanvas;
+
+    [SerializeField]
+    private CanvasGroup ObCanvas;
+    bool showObjective = true;
+
+    // Use this for initialization
+    void Awake()
+    {
+        initialCanvas.alpha = 0.8f;
+        initialCanvas.interactable = true;
+        ObCanvas.alpha = 0.5f;
+        ObCanvas.interactable = true;
+    }
+
+    void Start () {
+        
+	}
+    void hideCanvas()
+    {
+        ObCanvas.alpha = 0f;
+        ObCanvas.interactable = false;
+        ObCanvas.blocksRaycasts = false;
+       
+    }
+
+    void showCanvas()
+    {
+        ObCanvas.alpha = 0.5f;
+        ObCanvas.interactable = true;
+        ObCanvas.blocksRaycasts = true;
+       
+    }
+
+    public void OnButtonClick()
+    {
+        if (showObjective == false)
+        {
+            showObjective = true;
+        }
+        else if(showObjective == true)
+        {
+            showObjective = false;
+        }
+            
+    }
+	// Update is called once per frame
+	void Update () {
+        //    if(Input.GetKeyDown(KeyCode.O))
+        //       {
+        //           showObjective = false;
+        //       }
+        if (showObjective == false)
+        {
+            hideCanvas();
+        }
+        else if (showObjective == true)
+        {
+            showCanvas();
+        }
+    }
+}
