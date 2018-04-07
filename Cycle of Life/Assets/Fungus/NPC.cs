f6c063bb863793921b8afe156628a50c81a3afb9
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-04-07 04:22:08 -0400

    Linked Fungus with Unity Script.

---
+++ Cycle of Life/Assets/Fungus/NPC.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+[EventHandlerInfo("NPCs","Simple NPC","Executes on Collision")]
+public class NPC : EventHandler {
+    public bool click = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void Dialogue()
+    {
+        ExecuteBlock();
+    }
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            click = true;
+            Dialogue();
+        }
+        
+	}
+
+}
---
+++ Cycle of Life/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    public GameObject apple;
+    // Use this for initialization
+    void Start()
+    {
+        apple = GameObject.FindGameObjectWithTag("NPC");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (apple != null)
+        {
+            Debug.Log("Character");
+        }
+    }
+
+}
