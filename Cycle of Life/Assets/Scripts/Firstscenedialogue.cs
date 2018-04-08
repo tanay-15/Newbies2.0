ba27d69c40e2f8e992bd34b7c7353dac1d00cf7c
Tanay Joshi <tanay.joshi1990@gmail.com>
2018-04-08 01:16:59 -0400

    Basic Fungus

---
+++ Cycle of Life/Assets/Scripts/Firstscenedialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fungus;
+[CommandInfo("Start","starting flowchart","test")]
+public class Firstscenedialogue : Command {
+
+    public override void OnEnter()
+    {
+        ChangeScene();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
